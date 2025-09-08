937b31643bb76d889a6e8e0c250cdf069d30d3bd
Eduardo Matias Spacech <matiascruse@gmail.com>
2025-09-08 15:59:29 -0300

    Cambio a usar BD integrador1 y comentarios varios

--- src/main/java/dto/ProductoRecaudacionDTO.java
+++ src/main/java/dto/ProductoRecaudacionDTO.java
@@ -6,6 +6,7 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
+// Este dto lo usamos par a retornar los datos del producto que más recaudó
 public class ProductoRecaudacionDTO {
     private int idProducto;
     private String nombre;
