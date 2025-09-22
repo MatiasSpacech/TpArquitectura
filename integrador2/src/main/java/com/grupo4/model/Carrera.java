53297c1b145dd3f434a4bf5c309d584f484f92dc
Agustin Van Waarde <agusvanwaarde2006@gmail.com>
2025-09-22 16:50:07 -0300

    Entidad carrera y estudianteCarrera agregada

--- integrador2/src/main/java/com/grupo4/model/Carrera.java
+++ integrador2/src/main/java/com/grupo4/model/Carrera.java
@@ -1,5 +1,31 @@
 package com.grupo4.model;
 
-public class Carrera {
+import com.grupo4.model.EstudianteCarrera;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="carrera")
+public class Carrera {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false, length = 100)
+    private String nombre;
+    @OneToMany(mappedBy = "carrera",fetch = FetchType.LAZY)
+    private List<EstudianteCarrera> estudiantes;
+
+    public Carrera(String nombre){
+        this.nombre = nombre;
+        this.estudiantes = new ArrayList<>();
+    }
 }
+
--- integrador2/src/main/java/com/grupo4/model/EstudianteCarrera.java
+++ integrador2/src/main/java/com/grupo4/model/EstudianteCarrera.java
@@ -1,5 +1,21 @@
 package com.grupo4.model;
 
-public class EstudianteCarrera {
+import javax.persistence.*;
+import java.time.LocalDate;
 
+@Entity
+public class EstudianteCarrera {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private LocalDate fechaIngreso;
+    @Column(nullable = false)
+    private boolean graduado;
+    @ManyToOne
+    @JoinColumn(name = "id_estudiante",unique = true)
+    private Estudiante estudiante;
+    @ManyToOne
+    @JoinColumn(name = "id_carrera",unique = true)
+    private Carrera carrera;
 }
