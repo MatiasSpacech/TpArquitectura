14516f2171fbba96b55a933a38e7b4e5b81d94ee
Daniel Teruggi <sdteruggi@gmail.com>
2025-09-29 15:58:18 -0300

    actualizando

--- integrador2/src/main/java/com/grupo4/dto/CarreraDTO.java
+++ integrador2/src/main/java/com/grupo4/dto/CarreraDTO.java
@@ -1,5 +1,11 @@
 package com.grupo4.dto;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CarreraDTO {
-
+    private long id;
+    private int duracion;
+    private List<EstudianteCarrera> estudiantes;
 }
--- integrador2/src/main/java/com/grupo4/dto/EstudianteDTO.java
+++ integrador2/src/main/java/com/grupo4/dto/EstudianteDTO.java
@@ -1,6 +1,16 @@
 package com.grupo4.dto;
 
-public class EstudianteDTO {
+import com.grupo4.model.EstudianteCarrera;
 
+import java.util.List;
+
+public class EstudianteDTO {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private String ciudad;
+    private int nroLibreta;
+    private List<EstudianteCarrera> estudianteCarreraList;
 }
 
